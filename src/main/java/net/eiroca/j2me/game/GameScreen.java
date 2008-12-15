8c6a1d4960675c8ac42e1ab4eb66e07674bbf7f9
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2008-12-15 10:03:58 +0000

    Support game screen.

---
+++ src/main/java/net/eiroca/j2me/game/GameScreen.java
@@ -0,0 +1,89 @@
+/** GPL >= 2.0
+ *
+ * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+// Expand to define MIDP define
+@DMIDPVERS@
+package net.eiroca.j2me.game;
+
+import javax.microedition.lcdui.Graphics;
+//#ifdef DMIDP20
+import javax.microedition.lcdui.game.GameCanvas;
+//#else
+import javax.microedition.lcdui.Canvas;
+//#endif
+
+abstract public class GameScreen extends
+//#ifdef DMIDP20
+GameCanvas
+//#else
+Canvas
+{
+
+  protected final GameApp midlet;
+  protected final Graphics screen;
+  protected final int screenWidth;
+  protected final int screenHeight;
+  protected final boolean fullScreenMode;
+
+  protected boolean active = false;
+  protected GameThread animationThread;
+  public String name;
+  public Score score;
+
+  public GameScreen(final GameApp aMidlet, final boolean suppressKeys, final boolean fullScreen) {
+    super(suppressKeys);
+    midlet = aMidlet;
+    fullScreenMode = fullScreen;
+    setFullScreenMode(fullScreenMode);
+    score = new Score();
+    screen = getGraphics();
+    screenWidth = screen.getClipWidth();
+    screenHeight = screen.getClipHeight();
+  }
+
+  public void init() {
+    active = true;
+  }
+
+  public void show() {
+    setFullScreenMode(fullScreenMode);
+    animationThread = new GameThread(this);
+    animationThread.start();
+  }
+
+  abstract public boolean tick();
+
+  public void hide() {
+    if (animationThread != null) {
+      animationThread.stopped = true;
+      animationThread = null;
+      setFullScreenMode(false);
+    }
+  }
+
+  public void done() {
+    hide();
+    active = false;
+    score.endGame();
+  }
+
+  public final boolean isActive() {
+    return active;
+  }
+
+}
