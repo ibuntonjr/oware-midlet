0fa46a2bcf937a63cbc640c1c26de24067b75154
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2008-12-15 10:06:23 +0000

    Info class for name, category, and value.

---
+++ trunk/src/main/java/net/eiroca/j2me/util/Info.java
@@ -0,0 +1,36 @@
+/** GPL >= 2.0
+ * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package net.eiroca.j2me.util;
+
+public class Info {
+
+  public String category;
+  public String name;
+  public String value;
+
+  public Info(final String category, final String name, final String value) {
+    this.category = category;
+    this.name = name;
+    this.value = value;
+  }
+
+  public String toString() {
+    return name + ": " + value;
+  }
+
+}
