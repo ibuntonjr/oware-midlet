ba90b0827cb7d46ef14bea65123fd58381c3b826
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-02-03 07:30:26 +0000

    Added comments.  Allow MIDP 1.0.  Have CLDC 1.1 thread name start with T.

--- trunk/src/main/java/net/eiroca/j2me/game/GameScreen.java
+++ trunk/src/main/java/net/eiroca/j2me/game/GameScreen.java
@@ -16,28 +16,49 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+/**
+ * This was modified no later than 2009-01-29
+ */
 // Expand to define MIDP define
-@DMIDPVERS@
+//#define DMIDP20
+// Expand to define CLDC define
+//#define DCLDCV10
+// Expand to define logging define
+//#define DNOLOGGING
 package net.eiroca.j2me.game;
 
 import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
 //#ifdef DMIDP20
 import javax.microedition.lcdui.game.GameCanvas;
 //#else
-import javax.microedition.lcdui.Canvas;
+//@import javax.microedition.lcdui.Canvas;
 //#endif
 
+//#ifdef DLOGGING
+//@import net.sf.jlogmicro.util.logging.Logger;
+//@import net.sf.jlogmicro.util.logging.Level;
+//#endif
+
+/**
+	* Support game screen.  Has graphics screen, width, height, animation thread.
+	* Show, hide, store/restore data.
+	*/
 abstract public class GameScreen extends
 //#ifdef DMIDP20
 GameCanvas
 //#else
-Canvas
+//@Canvas
+//#endif
 {
 
   protected final GameApp midlet;
-  protected final Graphics screen;
-  protected final int screenWidth;
-  protected final int screenHeight;
+  protected Graphics screen = null;
+	//#ifdef DMIDP10
+//@  protected Image simage;
+	//#endif
+  protected int screenWidth;
+  protected int screenHeight;
   protected final boolean fullScreenMode;
 
   protected boolean active = false;
@@ -45,34 +66,114 @@ Canvas
   public String name;
   public Score score;
 
+  //#ifdef DLOGGING
+//@  private Logger logger = Logger.getLogger("GameScreen");
+//@  private boolean fineLoggable = logger.isLoggable(Level.FINE);
+//@  private boolean finerLoggable = logger.isLoggable(Level.FINER);
+//@  private boolean finestLoggable = logger.isLoggable(Level.FINEST);
+  //#endif
+
   public GameScreen(final GameApp aMidlet, final boolean suppressKeys, final boolean fullScreen) {
+		//#ifdef DMIDP20
     super(suppressKeys);
+		//#else
+//@    super();
+		//#endif
     midlet = aMidlet;
     fullScreenMode = fullScreen;
-    setFullScreenMode(fullScreenMode);
+		//#ifdef DMIDP20
+    super.setFullScreenMode(fullScreenMode);
+		//#endif
     score = new Score();
-    screen = getGraphics();
-    screenWidth = screen.getClipWidth();
-    screenHeight = screen.getClipHeight();
-  }
+	}
+
+	//#ifdef DMIDP10
+//@  /**
+//@   * Initialize graphics portion only. This is for MIDP 1.0 devices which
+//@	 * do not get screen info until paint.  It is also used by MIDP 2.x
+//@	 * to be consistent.  So, we allow it to be called from 2 places.
+//@   */
+//@	public Graphics initGraphics(int x, int y) {
+//@		simage = Image.createImage(x, y);
+//@		Graphics cscreen = simage.getGraphics();
+		//#ifdef DLOGGING
+//@		if (finestLoggable) {logger.finest("initGraphics simage,cscreen=" + simage + "," + cscreen);}
+		//#endif
+//@    screenWidth = x;
+//@    screenHeight = y;
+//@		return cscreen;
+//@  }
+	//#endif
+
+  /**
+   * Initialize graphics portion only. This is for MIDP 1.0 devices which
+	 * do not get screen info until paint.  It is also used by MIDP 2.x
+	 * to be consistent.  So, we allow it to be called from 2 places.
+   */
+	public Graphics initGraphics() {
+		//#ifdef DMIDP20
+		Graphics cscreen = getGraphics();
+    screenWidth = cscreen.getClipWidth();
+    screenHeight = cscreen.getClipHeight();
+		//#ifdef DLOGGING
+//@		if (finestLoggable) {logger.finest("initGraphics cscreen,screenWidth,screenHeight=" + cscreen + "," + screenWidth + "," + screenHeight);}
+		//#endif
+		return cscreen;
+		//#else
+//@		return initGraphics(super.getWidth(), super.getHeight());
+		//#endif
+	}
+
+	//#ifdef DMIDP10
+//@  /**
+//@   * Initialize graphics portion only. This is for MIDP 1.0 devices which
+//@	 * do not get screen info until paint.  So, we allow it to be called from
+//@	 * 2 places.
+//@   */
+//@	public void sizeChanged(int x, int y) {
+//@		screen = initGraphics(x, y);
+//@  }
+	//#endif
 
   public void init() {
     active = true;
+    if (screen == null) {
+			screen = initGraphics();
+		}
   }
 
   public void show() {
-    setFullScreenMode(fullScreenMode);
-    animationThread = new GameThread(this);
+		//#ifdef DMIDP20
+    super.setFullScreenMode(fullScreenMode);
+		//#endif
+		synchronized(this) {
+			//#ifdef DCLDCV11
+//@			animationThread = new GameThread(this, "TGameScreen");
+			//#else
+			animationThread = new GameThread(this);
+			//#endif
+		}
     animationThread.start();
   }
 
   abstract public boolean tick();
 
+  /**
+   * Executed when hidden.  Stop animation thread, turn off full screen.
+   */
   public void hide() {
-    if (animationThread != null) {
-      animationThread.stopped = true;
-      animationThread = null;
+		GameThread canimationThread = null;
+		synchronized(this) {
+			canimationThread = animationThread;
+		}
+    if (canimationThread != null) {
+      canimationThread.stopped = true;
+			synchronized(this) {
+				animationThread = null;
+			}
+			//#ifdef DMIDP20
       setFullScreenMode(false);
+			//#endif
     }
   }
 
@@ -80,6 +181,14 @@ Canvas
     hide();
     active = false;
     score.endGame();
+  }
+
+  public void wakeup(int tries) {
+		synchronized(this) {
+			if (animationThread != null) {
+				animationThread.wakeup(tries);
+			}
+		}
   }
 
   public final boolean isActive() {
