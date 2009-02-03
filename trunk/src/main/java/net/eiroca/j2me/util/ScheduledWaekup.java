6d2b83a25af527610b4834ec3502a93c07460a15
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-02-03 07:52:07 +0000

    Added comments.

--- trunk/src/main/java/net/eiroca/j2me/util/ScheduledWaekup.java
+++ trunk/src/main/java/net/eiroca/j2me/util/ScheduledWaekup.java
@@ -1,6 +1,9 @@
 /** GPL >= 2.0
  * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  *
+ * This software was modified 2008-12-14.  The original file was
+ * in mobilesuite.sourceforge.net project.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -15,16 +18,33 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+/**
+ * This was modified no later than 2009-01-29
+ */
+// Expand to define logging define
+//#define DNOLOGGING
 package net.eiroca.j2me.util;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+//#ifdef DLOGGING
+//@import net.sf.jlogmicro.util.logging.Logger;
+//@import net.sf.jlogmicro.util.logging.Level;
+//#endif
+
+/**
+	* Timer task.  
+	*/
 public class ScheduledWaekup extends TimerTask {
 
   public static Timer setup(final SchedulerNotify listener, final int delay) {
     final Timer myTimer = new Timer();
     myTimer.schedule(new ScheduledWaekup(listener), delay);
+		//#ifdef DLOGGING
+//@    Logger logger = Logger.getLogger("ScheduledWaekup");
+//@    logger.finest("Timer scheduled");
+		//#endif
     return myTimer;
   }
 
