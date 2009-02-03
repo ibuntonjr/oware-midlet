d47b70d218186f197ee31939d864828e02cd0954
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-02-03 07:06:07 +0000

    Added comments.

--- presrc/main/java/net/eiroca/j2me/app/Comparator.java
+++ presrc/main/java/net/eiroca/j2me/app/Comparator.java
@@ -16,6 +16,9 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+/**
+ * This was modified no later than 2009-01-29
+ */
 package net.eiroca.j2me.app;
 
 /**
