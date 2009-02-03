fbcaeaa15c9e0537225aa0643b0ea3cc2c9f41be
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-02-03 07:30:02 +0000

    More comments.

--- trunk/src/main/java/net/eiroca/j2me/app/Device.java
+++ trunk/src/main/java/net/eiroca/j2me/app/Device.java
@@ -16,10 +16,16 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+/**
+ * This was modified no later than 2009-01-29
+ */
 package net.eiroca.j2me.app;
 
 import java.util.Hashtable;
 
+/**
+	* Get and store device locale and platform.
+	*/
 public class Device {
 
   private static Device self = null;
