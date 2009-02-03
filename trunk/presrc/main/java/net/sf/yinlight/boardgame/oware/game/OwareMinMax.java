9bb34e55294a8b021db0663f7bf904dcf17ab97e
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-02-03 07:04:52 +0000

    Oware min/max from Ivory City.

--- trunk/presrc/main/java/net/sf/yinlight/boardgame/oware/game/OwareMinMax.java
+++ trunk/presrc/main/java/net/sf/yinlight/boardgame/oware/game/OwareMinMax.java
@@ -1,9 +1,5 @@
 /** GPL >= 2.0
  *
- * This software was modified 2008-12-09.  The original file was alphabet.c
- * in http://oware.ivorycity.com/. Also, some code was taken from 
- * GameMinMax.java in mobilesuite.sourceforge.net project.
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -17,6 +13,11 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/**
+ * This software was modified 2008-12-09.  The original file was alphabet.c
+ * in http://oware.ivorycity.com/. Also, some code was taken from 
+ * GameMinMax.java in mobilesuite.sourceforge.net project.
  */
 // Expand to define logging define
 @DLOGDEF@
@@ -110,7 +111,7 @@ public final class OwareMinMax extends GameMinMax {
 					bestmove.setPoint(0);
 				}
 
-				if( (depth<=0) || OwareGame.isGameEnded(testTable)) {
+				if( (depth<=0) || g.isGameEnded(g, testTable, player)) {
 					int tmpResult = OwareMinMax.heuristics[heuristic].getResult(player, testTable);
 					if (tmpResult > (byte)result) {
 						bestmove = new OwareMove(pMoves[i]);
