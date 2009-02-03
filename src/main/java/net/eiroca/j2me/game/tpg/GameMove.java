6deea3e8323aed69d3392d10953c12af9f3d1ce7
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-02-03 07:50:44 +0000

    Added comments.

--- src/main/java/net/eiroca/j2me/game/tpg/GameMove.java
+++ src/main/java/net/eiroca/j2me/game/tpg/GameMove.java
@@ -16,12 +16,18 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+/**
+ * This was modified no later than 2009-01-29
+ */
 package net.eiroca.j2me.game.tpg;
 
+/**
+	* Game move interface
+	*/
 public interface GameMove {
 
-  public int getPoint();
+  int getPoint();
 
-  public void setPoint(int point);
+  void setPoint(int point);
 
 }
