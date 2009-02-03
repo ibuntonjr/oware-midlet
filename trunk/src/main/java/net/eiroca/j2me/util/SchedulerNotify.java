581c3665812bbede5bccab1bcec4fd95addf67a7
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-02-03 07:52:15 +0000

    Added comments.

--- trunk/src/main/java/net/eiroca/j2me/util/SchedulerNotify.java
+++ trunk/src/main/java/net/eiroca/j2me/util/SchedulerNotify.java
@@ -15,10 +15,16 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+/**
+ * This was modified no later than 2009-01-29
+ */
 package net.eiroca.j2me.util;
 
+/**
+	* Scheduler notify interface.
+	*/
 public interface SchedulerNotify {
 
-  public void wakeup();
+  void wakeup();
 
 }
