93833589d13e5fc1bcc01b12fb13222353998980
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-02-03 07:30:41 +0000

    Added comments.

--- src/main/java/net/eiroca/j2me/game/GameUIMessage.java
+++ src/main/java/net/eiroca/j2me/game/GameUIMessage.java
@@ -2,6 +2,9 @@
  *
  * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  *
+ * This software was modified 2008-12-14.  The original file was
+ * in mobilesuite.sourceforge.net project.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -16,6 +19,13 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+/**
+ * This was modified no later than 2009-01-29
+ */
+// Expand to define MIDP define
+//#define DMIDP20
+// Expand to define logging define
+//#define DNOLOGGING
 package net.eiroca.j2me.game;
 
 import javax.microedition.lcdui.Canvas;
@@ -24,15 +34,31 @@ import javax.microedition.lcdui.Font;
 import javax.microedition.lcdui.Graphics;
 import net.eiroca.j2me.app.BaseApp;
 
+//#ifdef DLOGGING
+//@import net.sf.jlogmicro.util.logging.Logger;
+//@import net.sf.jlogmicro.util.logging.Level;
+//#endif
+
+/**
+	* Game message (canvas) with displayable for next.
+	*/
 public class GameUIMessage extends Canvas {
 
   private final String[] msg;
   Displayable next;
 
+	//#ifdef DLOGGING
+//@	private Logger logger = Logger.getLogger("GameUIMessage");
+//@  private boolean fineLoggable = logger.isLoggable(Level.FINE);
+//@  private boolean finestLoggable = logger.isLoggable(Level.FINEST);
+	//#endif
+
   public GameUIMessage(final String[] msg, final Displayable next) {
     super();
     this.msg = msg;
+		//#ifdef DMIDP20
     setFullScreenMode(true);
+		//#endif
     this.next = next;
   }
 
@@ -54,20 +80,34 @@ public class GameUIMessage extends Canvas {
   }
 
   private void drawText(final Graphics g, final int centerX, final int centerY) {
-    final int l = msg.length;
-    final int fontHeight = g.getFont().getHeight();
-    final int textHeight = l * fontHeight;
-    int topY = centerY - textHeight / 2;
-    for (int i = 0; i < l; i++) {
-      if (msg[i] != null) {
-        g.drawString(msg[i], centerX, topY, Graphics.HCENTER | Graphics.TOP);
-      }
-      topY = topY + fontHeight;
-    }
+		try {
+			final int l = msg.length;
+			final int fontHeight = g.getFont().getHeight();
+			final int textHeight = l * fontHeight;
+			int topY = centerY - textHeight / 2;
+			for (int i = 0; i < l; i++) {
+				if (msg[i] != null) {
+					g.drawString(msg[i], centerX, topY, Graphics.HCENTER | Graphics.TOP);
+				}
+				topY = topY + fontHeight;
+			}
+		} catch (Throwable e) {
+			e.printStackTrace();
+			//#ifdef DLOGGING
+//@			logger.severe("drawText error", e);
+			//#endif
+		}
   }
 
   public void keyPressed(final int keyCode) {
-    BaseApp.back(null, next, true);
+		try {
+			BaseApp.back(null, next, true);
+		} catch (Throwable e) {
+			e.printStackTrace();
+			//#ifdef DLOGGING
+//@			logger.severe("keyPressed error", e);
+			//#endif
+		}
   }
 
 }
