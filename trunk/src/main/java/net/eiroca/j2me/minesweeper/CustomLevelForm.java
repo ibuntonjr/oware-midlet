0399038ffc429c3475a7d771c42efd5b6f84d4b4
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-03-10 23:08:08 +0000

    Customize minesweeper properties.

---
+++ trunk/src/main/java/net/eiroca/j2me/minesweeper/CustomLevelForm.java
@@ -0,0 +1,45 @@
+/** GPL >= 2.0
+ * Based upon J2ME Minesweeper.
+ * Copyright (C) M. Jumari
+ * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package net.eiroca.j2me.minesweeper;
+
+import net.eiroca.j2me.minesweeper.MineSweeper;
+import javax.microedition.lcdui.TextField;
+import net.eiroca.j2me.app.BaseApp;
+import com.substanceofcode.rssreader.presentation.FeatureForm;
+
+public class CustomLevelForm extends FeatureForm {
+
+  private final TextField tBomb;
+
+  public CustomLevelForm() {
+    super(BaseApp.messages[MineSweeper.MSG_CUSTOMLEVEL]);
+    tBomb = new TextField(BaseApp.messages[MineSweeper.MSG_CL_BOMBS], "", 2, TextField.NUMERIC);
+    append(tBomb);
+  }
+
+  public void setInputs() {
+    tBomb.setString(Integer.toString(MineSweeper.bomb));
+  }
+
+  public void getInputs() {
+    MineSweeper.bomb = Integer.parseInt(tBomb.getString());
+  }
+
+}
