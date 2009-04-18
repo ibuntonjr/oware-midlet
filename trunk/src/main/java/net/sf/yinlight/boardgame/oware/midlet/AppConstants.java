4cc8c03bd03e5c5650338b1422cbc67fc0791973
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-04-18 06:59:58 +0000

    Allow creating a program with just one app.

---
+++ trunk/src/main/java/net/sf/yinlight/boardgame/oware/midlet/AppConstants.java
@@ -0,0 +1,67 @@
+/** GPL >= 2.0
+ * Based upon jtReversi game written by Jataka Ltd.
+ *
+ * This software was modified 2008-12-07.  The original file was Reversi.java
+ * in mobilesuite.sourceforge.net project.
+ *
+ * Copyright (C) 2002-2004 Salamon Andras
+ * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+/**
+ * This was modified no later than 2009-01-29
+ */
+// Expand to define MIDP define
+//#define DMIDP20
+// Expand to define test define
+//#define DNOTEST
+// Expand to define JMUnit test define
+//#define DNOJMTEST
+// Expand to define test ui define
+//#define DNOTESTUI
+// Expand to define logging define
+//#define DNOLOGGING
+package net.sf.yinlight.boardgame.oware.midlet;
+
+import net.sf.yinlight.boardgame.oware.game.BoardGameApp;
+
+/**
+	* Game application constants.
+	*/ 
+public class AppConstants {
+
+  public static short msgOffset = 0;
+  final public static int MSG_OWARE_NAME = BoardGameApp.MSG_USERDEF + msgOffset++; // 0
+  final public static int MSG_INIT_SEEDS = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_MAX_HOUSES = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_GRAND_SLAM = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_GRAND_SLAM1 = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_GRAND_SLAM2 = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_GRAND_SLAM3 = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_GRAND_SLAM4 = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_GRAND_SLAM5 = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_GRAND_SLAM6 = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_OPPONENT_EMPTY = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_OPPONENT_EMPTY1 = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_OPPONENT_EMPTY2 = BoardGameApp.MSG_USERDEF + msgOffset++;
+  final public static int MSG_OWARE_USERDEF = BoardGameApp.MSG_USERDEF + msgOffset;
+  final public static int MSG_REVERSI_NAME = BoardGameApp.MSG_USERDEF + msgOffset++; // 0
+  final public static int MSG_UPPER = BoardGameApp.MSG_USERDEF + msgOffset++; // 0
+  final public static int MSG_LOWER = BoardGameApp.MSG_USERDEF + msgOffset++; // 0
+  final public static int MSG_REVERSI_USERDEF = BoardGameApp.MSG_USERDEF + msgOffset++; // 0
+
+}
