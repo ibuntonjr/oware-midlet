4495fa6bbb4c6e94783d6c58347e871152ae9349
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-04-21 09:55:22 +0000

    Class for testsuite.

---
+++ presrc/main/java/net/eiroca/j2me/testsuite/testutil/benchmark/PrecisionSuite.java
@@ -0,0 +1,39 @@
+/** GPL >= 2.0
+ *
+ * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+/**
+ * This was modified no later than 2009-04-20 by Irving Bunton, Jr
+ */
+// Expand to define memory size define
+@DMEMSIZEDEF@
+//#ifdef DLARGEMEM
+package net.eiroca.j2me.testsuite.testutil.benchmark;
+
+public class PrecisionSuite extends SuiteAbstract {
+
+  public static final String PREFIX = "B.P.";
+  public static final String CATEGORY = "Precision";
+
+  public PrecisionSuite() {
+    super(PrecisionSuite.CATEGORY, PrecisionSuite.PREFIX);
+    benchmark = new BenchmarkAbstract[1];
+    benchmark[0] = new Precision(this);
+  }
+
+}
+//#endif
