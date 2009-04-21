de4c1c844a2506a5c19cb4f639370f1fead699dd
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-04-21 09:55:34 +0000

    Class for testsuite.

---
+++ presrc/main/java/net/eiroca/j2me/testsuite/testutil/benchmark/SuiteAbstract.java
@@ -0,0 +1,33 @@
+/**
+ * This was modified no later than 2009-04-20 by Irving Bunton, Jr
+ */
+// Expand to define memory size define
+@DMEMSIZEDEF@
+//#ifdef DLARGEMEM
+package net.eiroca.j2me.testsuite.testutil.benchmark;
+
+import net.eiroca.j2me.testsuite.testutil.AbstractProcessor;
+
+public class SuiteAbstract extends AbstractProcessor implements Runnable {
+
+  protected BenchmarkAbstract[] benchmark;
+  public boolean finished = false;
+
+  public SuiteAbstract(final String cat, final String prefix) {
+    super(cat, prefix);
+  }
+
+  public void execute() {
+    finished = false;
+    new Thread(this).start();
+  }
+
+  public void run() {
+    for (int i = 0; i < benchmark.length; i++) {
+      benchmark[i].execute();
+    }
+    finished = true;
+  }
+
+}
+//#endif
