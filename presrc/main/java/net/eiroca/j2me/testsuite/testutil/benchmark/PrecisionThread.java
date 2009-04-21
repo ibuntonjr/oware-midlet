11041bc5e18e4e2a91e6bc08cf037e6df09be849
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-04-21 09:55:28 +0000

    Class for testsuite.

---
+++ presrc/main/java/net/eiroca/j2me/testsuite/testutil/benchmark/PrecisionThread.java
@@ -0,0 +1,53 @@
+/** MIT LICENSE
+ * Based upon Mobile Device Tools written by Andrew Scott
+ *
+ * Copyright (C) 2004 Andrew Scott
+ * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/**
+ * This was modified no later than 2009-04-20 by Irving Bunton, Jr
+ */
+// Expand to define memory size define
+@DMEMSIZEDEF@
+//#ifdef DLARGEMEM
+package net.eiroca.j2me.testsuite.testutil.benchmark;
+
+public class PrecisionThread extends Thread {
+
+  public long iBefore;
+  public long iAfter;
+  public boolean finished = false;
+
+  public void run() {
+
+    iBefore = System.currentTimeMillis();
+    try {
+      Thread.sleep(1); // sleep for 1 ms
+    }
+    catch (final InterruptedException ie) {
+      // ignore
+    }
+    iAfter = System.currentTimeMillis();
+    finished = true;
+  }
+
+}
+//#endif
