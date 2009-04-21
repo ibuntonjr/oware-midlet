51adc8d1c83d5ce0845f3df1697d530cf8576260
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-04-21 09:59:15 +0000

    Check in missing classes.

---
+++ trunk/src/main/java/net/eiroca/j2me/util/HTTPAttach.java
@@ -0,0 +1,17 @@
+
+/**
+ * This was modified 2009-04-20 by Irving Bunton, Jr
+ */
+// Expand to define memory size define
+//#define DREGULARMEM
+//#ifdef DLARGEMEM
+//@package net.eiroca.j2me.util;
+//@
+//@public interface HTTPAttach {
+//@
+//@  String getMimeType();
+//@
+//@  byte[] getData();
+//@
+//@}
+//#endif
