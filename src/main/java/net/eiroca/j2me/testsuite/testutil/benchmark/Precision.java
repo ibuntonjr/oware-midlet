185510c2897307b8d6322add5598ce8423057886
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-04-21 10:55:54 +0000

    Add testsuite classes.

---
+++ src/main/java/net/eiroca/j2me/testsuite/testutil/benchmark/Precision.java
@@ -0,0 +1,57 @@
+/** MIT LICENSE
+ * Based upon Mobile Device Tools written by Andrew Scott
+ *
+ * Copyright (C) 2004 Andrew Scott
+ * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/**
+ * This was modified no later than 2009-04-20 by Irving Bunton, Jr
+ */
+// Expand to define memory size define
+//#define DREGULARMEM
+//#ifdef DLARGEMEM
+//@package net.eiroca.j2me.testsuite.testutil.benchmark;
+//@
+//@public class Precision extends BenchmarkAbstract {
+//@
+//@  public static final String RES_ID1 = "S";
+//@
+//@  public Precision(final SuiteAbstract suite) {
+//@    super(suite);
+//@  }
+//@
+//@  public void execute() {
+//@    // Retrieve the minimum resolution timers can measure
+//@    long iResolution = 0;
+//@    final PrecisionThread tSleeper = new PrecisionThread();
+//@    tSleeper.start();
+//@    try {
+//@      tSleeper.join();
+//@    }
+//@    catch (final InterruptedException ie) {
+//@      // ignore
+//@    }
+//@    iResolution = tSleeper.iAfter - tSleeper.iBefore;
+//@    suite.addResult(Precision.RES_ID1, Long.toString(iResolution));
+//@  }
+//@
+//@}
+//#endif
