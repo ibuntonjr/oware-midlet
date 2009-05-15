3e1d03c6c45d2ea1e6d0b2014692c32ff2d6930d
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-05-15 10:30:50 +0000

    Conditional compile for JSR120.

--- trunk/presrc/main/java/net/eiroca/j2me/sm/ui/PINChangeScreen.java
+++ trunk/presrc/main/java/net/eiroca/j2me/sm/ui/PINChangeScreen.java
@@ -15,6 +15,16 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+/**
+ * Modification started 2009-05-13.
+ */
+// Expand to define JSR-120 test define
+@DJSR120@
+// Expand to define JMUnit test define
+@DJMTESTDEF@
+// Expand to define logging define
+@DLOGDEF@
+//#ifdef DJSR120
 package net.eiroca.j2me.sm.ui;
 
 import net.eiroca.j2me.sm.midlet.SecureSMS;
@@ -60,3 +70,4 @@ public class PINChangeScreen extends Form {
   }
 
 }
+//#endif
