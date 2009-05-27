27812aa485d26b0a57bae722837737fe903cca18
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-05-27 11:30:58 +0000

    Comments.  Compile bluetooth only for JSR-82.

--- presrc/main/java/org/mega/gasp/bluetooth/miniplatform/bombergasp/DoItems.java
+++ presrc/main/java/org/mega/gasp/bluetooth/miniplatform/bombergasp/DoItems.java
@@ -20,6 +20,13 @@
  *
  * Author: Sebire - Negre
  */
+/**
+ * This was modified no later than 2009-05-26 by Irving Bunton, Jr
+ */
+
+// Expand to define DJSR82 define
+@DJSR82@
+//#ifdef DJSR82
 package org.mega.gasp.bluetooth.miniplatform.bombergasp;
 
 /**
@@ -47,3 +54,4 @@ public class DoItems {
         Utils.logGASPBT(" ");
     }
 }
+//#endif
