eb2c2e06c0fe1ca85e95168a9c9175477a79ba96
ibuntonjr <ibuntonjr@968f3a86-c90c-11dd-8ca8-e3ff79f713b6>
2009-05-27 11:30:45 +0000

    Comments.  Compile bluetooth only for JSR-82.

--- trunk/presrc/main/java/org/mega/gasp/bluetooth/miniplatform/bombergasp/DeleteBonus.java
+++ trunk/presrc/main/java/org/mega/gasp/bluetooth/miniplatform/bombergasp/DeleteBonus.java
@@ -20,6 +20,13 @@
  *
  * Author: Sebire - Negre
  */
+/**
+ * This was modified no later than 2009-05-26 by Irving Bunton, Jr
+ */
+
+// Expand to define DJSR82 define
+@DJSR82@
+//#ifdef DJSR82
 package org.mega.gasp.bluetooth.miniplatform.bombergasp;
 
 /**
@@ -52,3 +59,4 @@ public class DeleteBonus {
 	}
 	
 }
+//#endif
